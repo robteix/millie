58af41f1080f819977e88dfb0c487bf20a672a28
Roberto Teixeira <robteix@gmail.com>
2016-03-20 16:00:37 -0300

    Move badge logic to main process

--- app/utils/ui.js
+++ app/utils/ui.js
@@ -4,47 +4,13 @@
 import {getCurrentWindow, app, Menu} from 'remote';
 import {selectService} from '../actions/talky';
 
+const ipcRenderer = require('electron').ipcRenderer;
+
 export function setBadge(unread) {
   let text = '';
   if (unread > 0) text = ''+unread;
-  if (process.platform === "darwin") {
-    app.dock.setBadge("" + text);
-  } else if (process.platform === "win32") {
-    var win = remote.getCurrentWindow();
-
-    if (text === "") {
-      win.setOverlayIcon(null, "");
-      return;
-    }
-
-    // Create badge
-    var canvas = document.createElement("canvas");
-    canvas.height = 140;
-    canvas.width = 140;
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.ellipse(70, 70, 70, 70, 0, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.textAlign = "center";
-    ctx.fillStyle = "white";
-
-    if (text.length > 2) {
-      ctx.font = "75px sans-serif";
-      ctx.fillText("" + text, 70, 98);
-    } else if (text.length > 1) {
-      ctx.font = "100px sans-serif";
-      ctx.fillText("" + text, 70, 105);
-    } else {
-      ctx.font = "125px sans-serif";
-      ctx.fillText("" + text, 70, 112);
-    }
-
-    var badgeDataURL = canvas.toDataURL();
-    var img = NativeImage.createFromDataUrl(badgeDataURL);
-
-    win.setOverlayIcon(img, text);
-  }
+  console.log('set-badge', text);
+  ipcRenderer.sendSync('set-badge', text);
 }
 
 export function makeMenus(services, selectService) {
--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 'use strict';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-process.env.NODE_ENV = 'development';
 
 const electron = require('electron');
 const remote = electron.remote;
@@ -11,22 +10,26 @@ const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const crashReporter = electron.crashReporter;
 const shell = electron.shell;
+const ipcMain = require('electron').ipcMain;
+const nativeImage = require('electron').nativeImage;
 
 var ElectronSettings = require('electron-settings');
 var settings = new ElectronSettings({
     configDirPath: app.getPath('userData')
 });
 
+let mainWindow;
 crashReporter.start();
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')();
 }
 
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
+
 
 // logs
 // for writing error logs
@@ -43,7 +46,7 @@ process.on('uncaughtException', function (error) {
 });
 
 app.on('ready', () => {
-  let mainWindow = new BrowserWindow({ width: 1024, height: 728 });
+  mainWindow = new BrowserWindow({ width: 1024, height: 728 });
 
   mainWindow.loadURL(`file://${__dirname}/app/app.html`);
 
@@ -55,3 +58,55 @@ app.on('ready', () => {
     mainWindow.openDevTools();
   }
 });
+
+ipcMain.on('set-badge', function(event, arg) {
+  console.log('set-badge XX', arg);  // prints "ping"
+  event.returnValue = 'ok';
+  if (!mainWindow) return;
+  let text = arg;
+  if (text > 0) text = ''+arg;
+  if (process.platform === "darwin") {
+    app.dock.setBadge("" + text);
+  } else if (process.platform === "win32") {
+    var win = mainWindow;
+
+    if (text === "") {
+      win.setOverlayIcon(null, "");
+      return;
+    }
+
+    // Create badge
+    var canvas = document.createElement("canvas");
+    canvas.height = 140;
+    canvas.width = 140;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.ellipse(70, 70, 70, 70, 0, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+
+    if (text.length > 2) {
+      ctx.font = "75px sans-serif";
+      ctx.fillText("" + text, 70, 98);
+    } else if (text.length > 1) {
+      ctx.font = "100px sans-serif";
+      ctx.fillText("" + text, 70, 105);
+    } else {
+      ctx.font = "125px sans-serif";
+      ctx.fillText("" + text, 70, 112);
+    }
+
+    var badgeDataURL = canvas.toDataURL();
+    var img = nativeImage.createFromDataUrl(badgeDataURL);
+
+    win.setOverlayIcon(img, text);
+  }
+});
+
+
+/*
+
+
+*/
