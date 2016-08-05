dc084f2cd94f8bf78ddd3f38f90731e438302ba8
Roberto Teixeira <robteix@gmail.com>
2016-08-05 10:37:39 -0700

    Add script to generate release files

---
+++ distro.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DISTDIR="`pwd`/release"
+VERSION=0.9.4
+ELECTRON_VERSION=1.2.7
+
+
+
+create_package() {
+	os="$1"
+	arch="$2"
+	icon="$3"
+	rm -rf "$DISTDIR/Millie-${os}-${arch}.zip"
+	./node_modules/.bin/electron-packager . "Millie" --platform=$os --arch=$arch --icon=$icon --overwrite --prune --ignore=release --ignore=distro.sh --app-version="$VERSION" --app-copyright "Copyright (C) 2016 Roberto Selbach Teixeira" --asar --out="$DISTDIR" --version="$ELECTRON_VERSION"
+	cd "$DISTDIR/Millie-${os}-${arch}"
+	zip -9ryv "$DISTDIR/Millie-${os}-${arch}.zip" .
+	cd -
+}
+
+# max
+create_package darwin x64 ./app/img/app.icns
+create_package linux ia32 ./app/img/app.ico
+create_package linux x64 ./app/img/app.ico
+create_package win32 ia32 ./app/img/app.ico
+create_package win32 x64 ./app/img/app.ico
+grunt create-windows-installer
+
